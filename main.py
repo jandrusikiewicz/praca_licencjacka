efb4c8717312b7def99ccce1c71b7b0ed5f0742f
Jan A <janandrusikiewicz@gmail.com>
2022-04-17 12:11:51 +0200

    downloading by ID works

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import requests
 
 def get_variables(subject_id):
     url = f'https://bdl.stat.gov.pl/api/v1/variables?subject-id={subject_id}'
-
     response = requests.get(url)
     data = json.loads(response.content)
     data_normalized = pd.json_normalize(data, record_path='results')
@@ -16,27 +15,41 @@ def get_variables(subject_id):
 
 
 def get_data(variable_id, unit_level='5', year='2020', column_name="Data"):
-    url = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{variable_id}?format=json&unit-level={unit_level}&year={year}&page-size=100'
-    df = pd.DataFrame()
-    response = requests.get(url)
-    data = json.loads(response.content)
-    data_normalized = pd.json_normalize(data['results'], record_path='values', meta=['id', 'name'])
-    data_normalized.drop(columns=['attrId', 'id'], inplace=True)
-    url = data['links']['next']
-    df = pd.concat([df, data_normalized])
+    # response = requests.get(url)
+    # data = json.loads(response.content)
+    # data_normalized = pd.json_normalize(data['results'], record_path='values', meta=['id', 'name'])
+    # data_normalized.drop(columns=['attrId', 'id'], inplace=True)
+    # url = data['links']['next']
+    # df = pd.concat([df, data_normalized])
 
-    while data['links']['self'] is not data['links']['last']:
+    url = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{variable_id}?format=json&unit-level={unit_level}&year={year}&page-size=100'
+    url1 = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{variable_id}?format=json&unit-level={unit_level}&year={year}&page-size=100'
+    df = pd.DataFrame()
+    counter = 0
+    page = 0
+
+    while counter < 1:
         try:
             response = requests.get(url)
             data = json.loads(response.content)
             data_normalized = pd.json_normalize(data['results'], record_path='values', meta=['id', 'name'])
             data_normalized.drop(columns=['attrId', 'id'], inplace=True)
-
-            url = data['links']['next']
-
             df = pd.concat([df, data_normalized])
+
+            print(data['links']['self'])
+            print(counter)
+            print(page)
+
+            if url == data['links']['last']:
+                counter += 1
+
+
+
         except Exception:
             pass
+
+        page += 1
+        url = url1 + '&page=' + str(page)
 
         time.sleep(1)
 
@@ -44,17 +57,4 @@ def get_data(variable_id, unit_level='5', year='2020', column_name="Data"):
 
 
 # print(get_variables('P3783'))
-# print(get_data('00009').head(1000))
-
-
-# dziala!!!!!!
-response = requests.get(
-    'https://bdl.stat.gov.pl/api/v1/data/by-variable/00009?format=json&year=2004&year=2005&year=2006&page=0&page-size=100')
-data = json.loads(response.content)
-df = pd.DataFrame()
-
-# data_normalized = pd.json_normalize(data['results'], record_path='values', meta=['id', 'name'])
-# data_normalized.drop(columns=['attrId', 'id'], inplace=True)
-# url = data['links']['next']
-# df = pd.concat([df, data_normalized])
-print(data)
+print(get_data('633617').head(1000))
