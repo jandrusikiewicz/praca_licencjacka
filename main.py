9d489e89c04eab4929b0cd898667e485260b7036
Jan A <janandrusikiewicz@gmail.com>
2022-04-17 12:55:33 +0200

    have added downloading data from all variables in subject

--- main.py
+++ main.py
@@ -14,13 +14,9 @@ def get_variables(subject_id):
     return data_normalized
 
 
-def get_data(variable_id, unit_level='5', year='2020', column_name="Data"):
-    # response = requests.get(url)
-    # data = json.loads(response.content)
-    # data_normalized = pd.json_normalize(data['results'], record_path='values', meta=['id', 'name'])
-    # data_normalized.drop(columns=['attrId', 'id'], inplace=True)
-    # url = data['links']['next']
-    # df = pd.concat([df, data_normalized])
+def get_data_by_variable(variable_id: str = None, unit_level='5', year='2020'):
+    if variable_id is None:
+        variable_id = ''
 
     url = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{variable_id}?format=json&unit-level={unit_level}&year={year}&page-size=100'
     url1 = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{variable_id}?format=json&unit-level={unit_level}&year={year}&page-size=100'
@@ -36,25 +32,36 @@ def get_data(variable_id, unit_level='5', year='2020', column_name="Data"):
             data_normalized.drop(columns=['attrId', 'id'], inplace=True)
             df = pd.concat([df, data_normalized])
 
-            print(data['links']['self'])
-            print(counter)
-            print(page)
+            # print(data['links']['self'])
+            # print(counter)
+            # print(page)
 
             if url == data['links']['last']:
                 counter += 1
-
-
 
         except Exception:
             pass
 
         page += 1
         url = url1 + '&page=' + str(page)
-
         time.sleep(1)
 
     return df
 
 
-# print(get_variables('P3783'))
-print(get_data('633617').head(1000))
+def get_all_data_from_subject(subject_id):
+    subject_df = get_variables(subject_id)
+    df = pd.DataFrame()
+
+    for variable in subject_df.itertuples():
+        variable_df = get_data_by_variable(variable.id)
+        variable_df.rename(columns={'val': variable.n1 + ' ' + variable.n2}, inplace=True)
+
+        df = df.join(variable_df)
+
+    return df
+
+
+# print(get_variables('P3783').to_string())
+# print(get_data_by_variable('633617').head(1000))
+print(get_all_data_from_subject('P3783'))
